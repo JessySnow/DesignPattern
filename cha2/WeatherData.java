1a6043457028c9dc84459549050917596ac98152
JessySnow <jessysnow12345@gmail.com>
2022-03-17 20:18:05 +0800

    RESET

--- cha2/WeatherData.java
+++ cha2/WeatherData.java
@@ -1,8 +1,13 @@
 package cha2;
 import java.util.ArrayList;
-
 import cha2.myinterface.*;
 
+/**
+ * 实现了 Subject 的受观察者
+ * 该类的成员变量如下
+ * temperature、humidity、pressure 三个被观察的数据
+ * observers 存放 Observer对象 的数组
+ */
 public class WeatherData implements Subject{
     private ArrayList<Observer> observers;
     private float temperature;
@@ -14,17 +19,23 @@ public class WeatherData implements Subject{
         observers = new ArrayList<>();
     }
 
+    // 获取到一个新 Observer 的引用
     @Override
     public void registerObserver(Observer observer){
         observers.add(observer);
     }
 
+    // 删除了一个 Observer 的引用
     @Override
     public void removeObserver(Observer observer){
         if(observers.indexOf(observer) >= 0)
             observers.remove(observer);
     }
 
+    /**
+     * 将新消息推送给所有的 Observer 对象
+     * 因为有 Observer 对象的引用，所以可以将 消息 通过函数调用的方式，赋值给 Observer 对象
+     */
     @Override
     public void notifyObserver(){
         for(Observer obs : this.observers){
@@ -32,15 +43,19 @@ public class WeatherData implements Subject{
         }
     }
 
+
     public void measurementsChanged(){
         this.notifyObserver();
     }
 
+    /**
+     * 数据更改的方法，测试观察者模式
+     * 在数据更改后，立刻通知所有的观察者
+     */
     public void setMeasurements(float temperature, float humidity, float pressure){
         this.temperature = temperature;
         this.humidity = humidity;
         this.pressure = pressure;
-
         measurementsChanged();
     }
 }
--- cha2/weatherboard/CurrentConditionsDisplay.java
+++ cha2/weatherboard/CurrentConditionsDisplay.java
@@ -4,20 +4,32 @@ import cha2.myinterface.DisplayElement;
 import cha2.myinterface.Observer;
 import cha2.myinterface.Subject;
 
+/**
+ * 实现了 Observer 接口的观察者 (DisplayElement是一个策略接口，与观察者模式无关)
+ * 该类的成员变量如下
+ * temperature、humidity 存放接收到的推送消息的数据域
+ * weatherData 对象 持有的 Subject 对象，持有这个对象用于的解除注册，停止接受消息
+ */
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
     private float temperature;
     private float humidity;
     private Subject weatherData;
 
+    // 构造方法，将自己注册为一个观察者
     public CurrentConditionsDisplay(Subject weatherData){
         this.weatherData = weatherData;
         weatherData.registerObserver(this);
     }
 
+    /** 
+     * 实现了 Observer 接口的 update 方法
+     * 该方法会通过参数赋值的方法，将接收到的消息赋值给成员变量
+     * */    
     @Override
     public void update(float temperature, float humidity, float pressure){
         this.temperature = temperature;
         this.humidity = humidity;
+        // 这里直接调用 display 方法是为了更直观地测试观察者模式地效果
         display();
     }
 
